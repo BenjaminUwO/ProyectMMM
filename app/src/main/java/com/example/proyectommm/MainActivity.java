804df9a125c805f91e85de9a11ce482927347bba
BenjaminUwO <bennyuwo@gmail.com>
2022-04-22 15:50:13 -0500

    primer commit

---
+++ app/src/main/java/com/example/proyectommm/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.proyectommm;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.graphics.drawable.GradientDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button BtnValores, BtnNombre;
+    TextView LblSaludo, LblPuntValores;
+    EditText TxtNombre;
+    LinearLayout ContJuegos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BtnNombre = findViewById(R.id.BtnNombre);
+        BtnValores = findViewById(R.id.BtnValores);
+        LblSaludo = findViewById(R.id.LblSaludo);
+        LblPuntValores = findViewById(R.id.LblSaludo);
+        TxtNombre = findViewById(R.id.TxtNombre);
+        ContJuegos = findViewById(R.id.ContJuegos);
+
+        BtnNombre.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        String NombreUsuario;
+
+        switch (v.getId()){
+            case R.id.BtnNombre:
+                NombreUsuario = String.valueOf(TxtNombre.getText().toString());
+                LblSaludo.setText("Hola "+NombreUsuario);
+                ContJuegos.setVisibility(View.VISIBLE);
+                break;
+        }
+    }
+}
