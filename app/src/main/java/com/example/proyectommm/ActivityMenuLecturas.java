a4d70f68cb9d543b0d467166acc3e31069587f67
cloudluis22 <cloudluis22@hotmail.com>
2022-04-25 21:11:45 -0500

    Archivos para las lecturas

---
+++ app/src/main/java/com/example/proyectommm/ActivityMenuLecturas.java
@@ -0,0 +1,17 @@
+package com.example.proyectommm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityMenuLecturas extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_lecturas);
+    }
+}
