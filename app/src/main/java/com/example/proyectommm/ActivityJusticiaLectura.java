c63fbdb3f95e9935f915c9cc7b2548a62b7a76f6
cloudluis22 <cloudluis22@hotmail.com>
2022-04-26 22:12:36 -0500

    Terminadas actividades de menu de lectura y las lectura respectivamente.

---
+++ app/src/main/java/com/example/proyectommm/ActivityJusticiaLectura.java
@@ -0,0 +1,14 @@
+package com.example.proyectommm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityJusticiaLectura extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_justicia_lectura);
+    }
+}
